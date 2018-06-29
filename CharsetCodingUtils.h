a8d07df4f6f6f3534df8214df576f47bdf09b39f
paudy <zhangbd@gmail.com>
2018-06-29 10:11:14 +0800

    charset coding conversion utils

---
+++ CharsetCodingUtils.h
@@ -0,0 +1,185 @@
+//Unicode16תANSI
+string UnicodeToANSI(const wchar_t* str)
+{
+    char*     pElementText;
+    int    iTextLen;
+    // wide char to multi char
+    iTextLen = WideCharToMultiByte( CP_ACP,
+        0,
+        str,
+        -1,
+        NULL,
+        0,
+        NULL,
+        NULL );
+    pElementText = new char[iTextLen + 1];
+    memset( ( void* )pElementText, 0, sizeof( char ) * ( iTextLen + 1 ) );
+    ::WideCharToMultiByte( CP_ACP,
+        0,
+        str,
+        -1,
+        pElementText,
+        iTextLen,
+        NULL,
+        NULL );
+    string strText;
+    strText = pElementText;
+    delete[] pElementText;
+?
+    return strText;
+}
+
+//ANSIתUnicode16:
+wstring ANSIToUnicode(const string& str)
+{
+    int  len = 0;
+    len = str.length();
+    int  unicodeLen = ::MultiByteToWideChar( CP_ACP,
+        0,
+        str.c_str(),
+        -1,
+        NULL,
+        0 ); 
+    wchar_t *  pUnicode; 
+    pUnicode = new  wchar_t[unicodeLen+1]; 
+    memset(pUnicode,0,(unicodeLen+1)*sizeof(wchar_t)); 
+    ::MultiByteToWideChar( CP_ACP,
+        0,
+        str.c_str(),
+        -1,
+        (LPWSTR)pUnicode,
+        unicodeLen ); 
+    wstring  rt; 
+    rt = ( wchar_t* )pUnicode;
+    delete  pUnicode;
+?
+    return  rt; 
+}
+
+
+//Unicode16תUTF-8 :
+typedef unsigned __int32 uint32_t;
+typedef unsigned short  uint16_t;
+?
+uint32_t Unicode16ToUTF8(IN const uint16_t* pszUtf16, IN uint32_t nSizeUtf16, IN char* pszUtf8, IN uint32_t nSizeUtf8)  
+{  
+    if (0 == nSizeUtf8)
+    {
+        return (nSizeUtf16 * 3 + 1);
+    }
+?
+    uint32_t i = 0, count = 0;       
+    uint16_t wch;  
+    for(i=0; i < nSizeUtf16; i++) 
+    {
+        wch = *(uint16_t*)&pszUtf16[i];       
+        if( wch < 0x80)  
+        {
+            if ((count+1) >= nSizeUtf8)
+            {
+                break;
+            }
+            pszUtf8[count] = wch & 0x7f;  
+            count++;  
+        }  
+        else if( wch >= 0x80 && wch < 0x07ff)  
+        {
+            if ((count+2) >= nSizeUtf8)
+            {
+                break;
+            }
+            //tmp1 = wch >> 6;  
+            pszUtf8[count] = 0xC0 | (0x1F & wch>>6);  
+            pszUtf8[count+1] = 0x80 | (0x3F & wch);  
+            count += 2;  
+        }  
+        else if( wch>=0x0800 )  
+        {
+            if ((count+3) >= nSizeUtf8)
+            {
+                break;
+            }
+            //tmp1 = wch >> 12;  
+            pszUtf8[count] = 0xE0 | (0x0F & wch>>12);  
+            pszUtf8[count+1] = 0x80 | ((0x0FC0 & wch)>>6);  
+            pszUtf8[count+2] = 0x80 | (0x003F & wch);  
+             
+            count += 3;  
+        }     
+        else  
+        {  
+            printf("error/n");  
+        }  
+    }
+?
+    if (count < nSizeUtf8)
+    {
+        pszUtf8[count] = 0;
+        return (count + 1);
+    }
+    else if (nSizeUtf8 > 0)
+    {
+        pszUtf8[nSizeUtf8-1] = 0;
+        return nSizeUtf8;
+    }
+?
+    return 0;  
+}
+
+
+//UTF-8תUnicode16:
+uint32_t UTF8ToUnicode16(IN const unsigned char* pszUtf8, IN uint32_t nSizeUtf8, OUT uint16_t* pszUtf16, IN uint32_t nSizeUtf16)  
+{  
+    if (0 == nSizeUtf16)
+    {
+        return (nSizeUtf8 + 1);
+    }
+?
+    uint32_t count = 0, i = 0;  
+    uint16_t wch;  
+    uint16_t *p;  
+    for(i = 0; count < nSizeUtf8 && i < nSizeUtf16; i++) 
+    {
+        p = (uint16_t*)&pszUtf16[i];  
+         
+        if( pszUtf8[count] < 0x80)  
+        {  
+            wch = pszUtf8[count];             
+            count++;  
+        }  
+        else if( (pszUtf8[count] < 0xDF) && (pszUtf8[count] >= 0x80))  
+        {  
+            wch = pszUtf8[count] & 0x1F;  
+            wch = wch << 6;  
+            wch += pszUtf8[count+1] & 0x3F;  
+            count += 2;  
+        }  
+        else if( (pszUtf8[count] <= 0xEF) && (pszUtf8[count] >= 0xDF))  
+        {  
+            wch = pszUtf8[count] & 0x0F;  
+            wch = wch << 6;  
+            wch += pszUtf8[count+1] & 0x3F;  
+            wch = wch << 6;  
+            wch += pszUtf8[count+2] & 0x3F;  
+            count += 3;  
+        }  
+        else  
+        {  
+            printf("error!/n");  
+        }  
+        *p = wch;  
+    }  
+?
+    if (i < nSizeUtf16)
+    {
+        pszUtf16[i] = 0;
+        return (i + 1);
+    }
+    else if (nSizeUtf16 > 0)
+    {
+        pszUtf16[nSizeUtf16-1] = 0;
+        return nSizeUtf16;
+    }
+ 
+    return 0;  
+}  
